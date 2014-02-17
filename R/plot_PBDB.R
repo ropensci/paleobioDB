d34edf24e4a38eaeb66c389390502bfa41acd364
Sara_Varela <svarela@paleobiogeography.org>
2014-02-17 18:15:08 -0300

    changed pbdb plot

--- R/plot_PBDB.R
+++ R/plot_PBDB.R
@@ -1,90 +1,72 @@
-library(rgdal)
-library(ggplot2)
+query<- canis_0_2
+name<- "Canis"
+col="turquoise1" 
+dir="C:/Users/sara/Documents/_CIENCIAS/pbdb_paper"
 
-
-
-# read shapefile
-setwd ("C:/Users/sara/Documents/_CIENCIAS/pbdb/docs_desarrollo")
-wmap <- readOGR(dsn="ne_110m_land.shp", layer="ne_110m_land")
-grat <- readOGR("ne_110m_graticules_15.shp", layer="ne_110m_graticules_15")
-bbox <- readOGR("ne_110m_wgs84_bounding_box.shp", layer="ne_110m_wgs84_bounding_box")
-countries <- readOGR("ne_110m_admin_0_countries.shp", layer="ne_110m_admin_0_countries")
-
-# Winkel tripel projection
-countries_wintri <- spTransform(countries, CRS("+proj=wintri"))
-bbox_wintri <- spTransform(bbox, CRS("+proj=wintri"))
-wmap_wintri <- spTransform(wmap, CRS("+proj=wintri"))
-grat_wintri <- spTransform(grat, CRS("+proj=wintri"))
-
-
-#read data PBDB, dataframe, latlong, number of fossil record. 
-places <- readOGR("ne_110m_populated_places.shp", layer="ne_110m_populated_places")
-places_df <- as(places, "data.frame")
-places_robin_df <- project(cbind(places_df$LONGITUDE, places_df$LATITUDE), proj="+proj=wintri")
-places_robin_df <- as.data.frame(places_robin_df)
-names(places_robin_df) <- c("LONGITUDE", "LATITUDE")
-places_robin_df$POP2000 <- places_df$POP2000 
-
-# create a blank ggplot theme
-theme_opts <- list(theme(panel.grid.minor = element_blank(),
-                         panel.grid.major = element_blank(),
-                         panel.background = element_blank(),
-                         plot.background = element_rect(fill="white"),
-                         panel.border = element_blank(),
-                         axis.line = element_blank(),
-                         axis.text.x = element_blank(),
-                         axis.text.y = element_blank(),
-                         axis.ticks = element_blank(),
-                         axis.title.x = element_blank(),
-                         axis.title.y = element_blank(),
-                         plot.title = element_text(size=22)))
-
-
-# con meridianos y paralelos
-ggplot(bbox_wintri, aes(long,lat, group=group)) +
-  geom_polygon(fill="black") +
-  geom_polygon(data=countries_wintri, 
-               aes(long,lat, group=group, fill=hole)) +
-  geom_point(data=places_robin_df, 
-             aes(LONGITUDE, LATITUDE, 
-                 group=NULL, fill=NULL, 
-                 size=POP2000/2), 
-             color="#32caf6", alpha=0.5) +
-  geom_path(data=countries_wintri, 
-            aes(long,lat, group=group, fill=hole), 
-            color="grey50", size=0.3) +
-  # geom_path(data=grat_wintri, aes(long, lat, group=group, fill=NULL), linetype=3, color="grey60") +
-  labs(title="PBDB: Canis + Quaternary") +
-  coord_equal(ratio=1) +
-  theme_opts +
-  scale_fill_manual(values=c("white", "white"), guide="none") + # change colors & remove legend
-  scale_size_continuous(range=c(1,20), 
-                        guide="none") # change colors & remove legend
-
-p<- ggplot(bbox_wintri, aes(long,lat, group=group)) +
-  geom_polygon(fill="black") +
-  geom_polygon(data=countries_wintri, 
-               aes(long,lat, group=group, fill=hole)) +
-  geom_point(data=places_robin_df, 
-             aes(LONGITUDE, LATITUDE, 
-                 group=NULL, fill=NULL, 
-                 size=POP2000/2), 
-             color="#32caf6", alpha=0.5) +
-  geom_path(data=countries_wintri, 
-            aes(long,lat, group=group, fill=hole), 
-            color="grey50", size=0.3) +
-  # geom_path(data=grat_wintri, aes(long, lat, group=group, fill=NULL), linetype=3, color="grey60") +
-  labs(title="PBDB: Canis + Quaternary") +
-  coord_equal(ratio=1) +
-  theme_opts +
-  scale_fill_manual(values=c("white", "white"), guide="none") + # change colors & remove legend
-  scale_size_continuous(range=c(1,20), 
-                        guide="none") # change colors & remove legend
-
-plot (p)
-ggsave(plot=p, "map.png", width=12.5, height=8.25, dpi=300)
-
-
+plot_pbdb<- function (query, name, col="turquoise1", dir){
+    # read shapefile
+    wmap <- readOGR(dsn="maps/ne_110m_land.shp", layer="ne_110m_land")
+    bbox <- readOGR("maps/ne_110m_wgs84_bounding_box.shp", layer="ne_110m_wgs84_bounding_box")
+    countries <- readOGR("maps/ne_110m_admin_0_countries.shp", layer="ne_110m_admin_0_countries")
+    
+    # Winkel tripel projection
+    countries_wintri <- spTransform(countries, CRS("+proj=wintri"))
+    bbox_wintri <- spTransform(bbox, CRS("+proj=wintri"))
+    wmap_wintri <- spTransform(wmap, CRS("+proj=wintri"))
+    
+    #read data and project using winkel tripel projection
+    latlong <- project(cbind(query[,11], query[,12]), proj="+proj=wintri")
+    latlong<- as.data.frame (latlong)
+    names (latlong)<- c("lng", "lat")
+    counts<- ddply(latlong,.(lng,lat),nrow)
+   
+    # create a blank ggplot theme
+    theme_opts <- list(theme(panel.grid.minor = element_blank(),
+                             panel.grid.major = element_blank(),
+                             panel.background = element_blank(),
+                             plot.background = element_rect(fill="white"),
+                             panel.border = element_blank(),
+                             axis.line = element_blank(),
+                             axis.text.x = element_blank(),
+                             axis.text.y = element_blank(),
+                             axis.ticks = element_blank(),
+                             axis.title.x = element_blank(),
+                             axis.title.y = element_blank(),
+                             plot.title = element_text(size=22)))
+    
+  
+  
+  #plot (the size of the points is the number of records in the site)
+  
+  p<- ggplot(bbox_wintri, aes(long,lat, group=group)) +
+    
+    geom_polygon(fill="black") +
+    
+    geom_polygon(data=countries_wintri, 
+                 aes(long,lat, group=group, fill=hole)) +
+    
+    geom_point(data=counts, 
+               aes(counts$lng, counts$lat, 
+                   group=NULL, fill=NULL, 
+                   size=counts$V1), 
+               color=col, alpha=0.5) +
+    
+    geom_path(data=countries_wintri, 
+              aes(long,lat, group=group, fill=hole), 
+              color="grey50", size=0.3) +
+    
+    # geom_path(data=grat_wintri, aes(long, lat, group=group, fill=NULL), linetype=3, color="grey60") +
+    labs(title=name) +
+    coord_equal(ratio=1) +
+    theme_opts +
+    scale_fill_manual(values=c("white", "white"), guide="none") + # remove legend
+    scale_size_continuous(limits=c(1,20), guide="none") # remove legend
+  
+  plot (p)
+  
+  ggsave(plot=p, paste (dir,"/", name, ".png", sep=""), width=12.5, height=8.25, dpi=300)
+    
+}
 
 
 
--- pruebas_queries.R
+++ pruebas_queries.R
@@ -1,28 +1,17 @@
-
-pbdb_query_occurrence (1001)
-
+#' pbdb_query_occurrences is a highly flexible function. It allows all the parameters available in the PBDB API.
+#' 
 #' Complete list of paramenters is available in http://paleobiodb.org/data1.1/occs/list
 #' Here, for practical reasons we selected the most common filters that 
-#' paleontologists and ecologists might use
-#' 
+#' paleontologists and ecologists might use to run our examples.
 #' 
 #'@param taxon_name Return only records associated with the specified taxonomic name(s). 
 #'You may specify multiple names, separated by commas.
 #'@param base_name  Return only records associated with the specified taxonomic name(s), or any of their children. 
 #'You may specify multiple names, separated by commas.
-#' @param show 
-#' attr The attribution of this taxon (author and year) 
-#' app  The age of first and last appearance of this taxon from the occurrences recorded in this database 
-#' size	The number of subtaxa appearing in this database
+#'@param show to show extra variables (e.g. coords)
 #' 
-#' @param min_ma  Return only intervals that are at least this old
-#' @param max_ma	Return only intervals that are at most this old
-#' 
-#' 
-
-
 #' Set the basic query: 
-#' limit= "all" to download all the occurrences (by default the limit is 500) 
+#' limit= "all" to download all the occurrences (by default the API limit is 500) 
 #' vocab= "pbdb" to show the names of the variables complete (by default variables have short 3-letter names)
 #' base_name= "Canis" to download all the records from the taxon and subtaxa 
 #' (e.g., "Canis", "Canis lupus", "Canis mosbachensis", etc.)
@@ -30,10 +19,35 @@ pbdb_query_occurrence (1001)
 #' 
 #' 
 
-canis<- pbdb_query_occurrences (limit="all", 
+canis<- pbdb_query_occurrences (limit="100", 
                                 vocab= "pbdb", 
                                 base_name="Canis", 
                                 show="coords")
 
+# the function plot will plot the query and save a png file in your working directory
+plot_pbdb (query= canis, name="Canis", col="turquoise1",  
+           dir="C:/Users/sara/Documents/_CIENCIAS/pbdb_paper")
 
 
+query<- canis
+name<- "canis"
+col="#32caf6"
+
+# use min_ma and max_ma to add a filter to the data: minimum and maximum age (specified in Ma) 
+canis_0_2<- pbdb_query_occurrences (limit=100, 
+                                           vocab= "pbdb", 
+                                           base_name="Canis", 
+                                           min_ma = 0,
+                                           max_ma = 2,
+                                           show="coords")
+
+plot_pbdb (query= canis_0_2, name= "Canis (0-2 Ma)", 
+           dir="C:/Users/sara/Documents/_CIENCIAS/pbdb_paper")
+
+canis_quaternary<- pbdb_query_occurrences (limit="all", 
+                                    vocab= "pbdb", 
+                                    base_name="Canis", 
+                                    interval="Quaternary",
+                                    show="coords")
+
+plot_pbdb (canis_quaternary, "Canis Quaternary")
