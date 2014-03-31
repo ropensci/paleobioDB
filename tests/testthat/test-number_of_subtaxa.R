dd3ceb4380d67bcc9f95ef7368057f5b41cb8c1c
Javier González <javigzz@yahoo.es>
2014-03-31 18:37:48 -0300

    review on some tests

--- tests/testthat/test-number_of_subtaxa.R
+++ tests/testthat/test-number_of_subtaxa.R
@@ -1,10 +1,11 @@
 # test for number_of_subtaxa in pbdb
 
 context ("number_of_subtaxa")
-data<- read.table ("data/canidae_quat.csv", sep=",", header=T)
-response<- number_of_subtaxa(data)
 
 test_that("number_of_subtaxa output is a dataframe",{ 
-  expect_is(response, 'data.frame')
+	
+	data<- read.table ("../../data/canidae_quat.csv", sep=",", header=T)
+	response<- pbdb_subtaxa(data)
+	expect_is(response, 'data.frame')
 })
 
